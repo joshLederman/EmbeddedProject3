31e706c1f93861c763dc4ea61bc4f800b38ab82c
Josh Lederman <joshua@lederman.com>
2017-03-22 15:51:30 -0400

    Process Start

--- process.c
+++ process.c
@@ -1,3 +1,22 @@
 #include "3140_concur.h"
 #include <stdlib.h>
 #include <fsl_device_registers.h>
+
+process_t *current_process = NULL;
+
+struct process_t {
+	unsigned int * sp;
+	struct process_state nextProcess;
+}
+
+void process_start (void) {
+	
+	NVIC_EnableIRG(PIT0_IRQn); //Enables interrupts
+	
+	PIT_MCR = 1; //Enables standard timers
+	
+	process_begin();
+}
+	
+unsigned int * process_select (unsigned int *cursp) {
+	
