fcb9334530408287e5ed96707b71d536f3ecc87d
Josh Lederman <joshua@lederman.com>
2017-03-27 16:41:25 -0400

    Test Cases, final version of process.c

---
+++ test_case_3.c
@@ -0,0 +1,68 @@
+#include "3140_concur.h"
+#include "utils.h"
+
+void p1 (void)
+{
+	int i;
+	for (i=0; i < 100; i++) {
+		delay ();
+    LEDRed_Toggle();
+	}
+}
+
+void p2 (void)
+{
+	int i;
+	for (i=0; i < 10; i++) {
+		delay ();
+    LEDBlue_Toggle();
+	}
+}
+
+void p3 (void)
+{
+	int i;
+	for (i=0; i < 10; i++) {
+		delay ();
+		delay ();
+    LEDBlue_Toggle();
+	}
+}
+
+void p4 (void)
+{
+	int i;
+	for (i=0; i < 10; i++) {
+		delay ();
+		delay ();
+    LEDRed_Toggle();
+	}
+}
+
+int main (void)
+{
+ LED_Initialize();
+ 
+ if (process_create (p1,20) < 0) {
+ 	return -1;
+ }
+
+ if (process_create (p2,20) < 0) {
+ 	return -1;
+ }
+ 
+ if (process_create (p3,20) < 0) {
+ 	return -1;
+ }
+	
+ if (process_create (p4,20) < 0) {
+ 	return -1;
+ }
+ process_start ();
+ 
+ LEDGreen_On();
+ 
+ while (1) ;
+
+ return 0;
+}
